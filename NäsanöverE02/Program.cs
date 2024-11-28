0a02452c59cd4f63636966ad3268a67233482b8e
Gun-od Garig <gun-od.garig@elev.ga.ntig.se>
2024-11-28 15:52:26 +0100

    klar med Övning 2

--- NäsanöverE02/Program.cs
+++ NäsanöverE02/Program.cs
@@ -1,22 +1,49 @@
 ﻿
 
-// foreach (string leksak in leksaker)
-// {
-//     Console.WriteLine(leksaker);
-// }
+foreach (string leksak in leksaker)
+{
+    Console.WriteLine(leksaker);
+}
 
-// string[] klasskamrater = { "Amira", "Theodora", "Leo", "Zion", "Vilma" };
-// List<string> leksaker = new List<string> { "Gosedjur", "Lego", "Docka", "Bilar", "Spiderman mini figure" };
-// List<int> numer = new List<int> { 2, 4, 6, 8, 10 };
+string[] klasskamrater = { "Amira", "Theodora", "Leo", "Zion", "Vilma" };
+List<string> leksaker = new List<string> { "Gosedjur", "Lego", "Docka", "Bilar", "Spiderman mini figure" };
+List<int> numer = new List<int> { 2, 4, 6, 8, 10 };
 
-// for (int i = 0; i < 5; i++)
-// {
-//     Console.WriteLine($"{klasskamrater[i]} ger {leksaker[i]} betyget {numer[i]}");
-// }
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{klasskamrater[i]} ger {leksaker[i]} betyget {numer[i]}");
+}
 
-List<string> cities = new List<string> { }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+List<string> cities = new List<string>{};
+string stad;
+
+Console.WriteLine("Ange namn på städer, eller skriv exit för att avsluta");
+
+while (true) {
+
+    Console.WriteLine("Ange en stad: ");
+    stad = Console.ReadLine();
+
+
+
+    if (stad == "exit")
+    {
+        break;
+    }
+    cities.Add(stad);
+    Console.WriteLine($"Staden {stad} har lagts in i listan");
+
+    
+}
+
+ Console.WriteLine("Du har lagt till följande städer: ");
+foreach (string city in cities) 
+{
+    Console.WriteLine(city);
+}
 
 
 Console.ReadLine();
--- NäsanöverE02/obj/Debug/net8.0/NäsanöverE02.AssemblyInfo.cs
+++ NäsanöverE02/obj/Debug/net8.0/NäsanöverE02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NäsanöverE02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b1df9055b29a5ef3b28f2cfaa9e2f89455135e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("NäsanöverE02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NäsanöverE02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
