5b1df9055b29a5ef3b28f2cfaa9e2f89455135e8
Gun-od Garig <gun-od.garig@elev.ga.ntig.se>
2024-11-28 15:22:39 +0100

    first commit

---
+++ NäsanöverE02/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+// foreach (string leksak in leksaker)
+// {
+//     Console.WriteLine(leksaker);
+// }
+
+// string[] klasskamrater = { "Amira", "Theodora", "Leo", "Zion", "Vilma" };
+// List<string> leksaker = new List<string> { "Gosedjur", "Lego", "Docka", "Bilar", "Spiderman mini figure" };
+// List<int> numer = new List<int> { 2, 4, 6, 8, 10 };
+
+// for (int i = 0; i < 5; i++)
+// {
+//     Console.WriteLine($"{klasskamrater[i]} ger {leksaker[i]} betyget {numer[i]}");
+// }
+
+List<string> cities = new List<string> { }
+
+
+
+
+Console.ReadLine();
